42117a836d7807f7aa9e8e56af4e285313d3ed66
David <david.mendez@terrificminds.com>
2025-07-03 17:56:53 +0530

    added all the files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./Drag and Drop/**/*.{html,js}","./to-do/**/*.{html,js}","./Kanban Board/**/*.{html,js}"],
-  theme: {
-    extend: {backgroundImage:{'radial-blue': 'radial-gradient(circle, rgba(0,0,0,0.03) 0%, rgba(96,165,250,1) 100%)',} },
-  },
-  plugins: [],
-}
-
